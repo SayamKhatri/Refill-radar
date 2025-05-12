2d813fbcf725a7e65db701fd0c86f20678b9e917
SayamKhatri <khatri.say@northeastern.edu>
2025-05-12 11:19:05 -0400

    Updated IAM.tf

--- aws-backend/iam.tf
+++ aws-backend/iam.tf
@@ -66,3 +66,23 @@ resource "aws_iam_policy_attachment" "lambda_s3_read_access" {
   roles      = [aws_iam_role.lambda_exec_role.name]
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
+
+resource "aws_iam_role" "dms_vpc_role" {
+  name = "dms-vpc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "dms.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dms_vpc_access_policy" {
+  role       = aws_iam_role.dms_vpc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
+}
